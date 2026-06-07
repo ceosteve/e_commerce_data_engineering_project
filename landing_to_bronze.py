16033924fb27d27563f6371d6f7c9a053d1b3d72
Allan Mwangi <carsonallan71@gmail.com>
2026-06-07 19:36:37 +0300

    Fix access keys and Spark session builder

--- landing_to_bronze.py
+++ landing_to_bronze.py
@@ -1,1 +1,76 @@
-ECHO is on.
+import boto3
+from datetime import datetime
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from utils import s3_client, glue_client, athena_client, get_spark
+
+spark = get_spark()
+
+#Read data from landing folder in aws s3
+carts_df = spark.read.json('s3a://ecomerce-landing123/carts/')
+products_df = spark.read.json('s3a://ecomerce-landing123/products/')
+users_df = spark.read.json('s3a://ecomerce-landing123/users/')
+
+# Getting headers for carts
+def cart_header_create(carts_header):
+  bronze_cart_header = carts_header.select(
+      col("id").cast("int").alias("cart_id"),
+      col("userId").cast("int").alias("user_id"),
+      col("total").cast("double"),
+      col("discountedTotal").cast("double"),
+      col("totalProducts").cast("int"),
+      col("totalQuantity").cast("int")
+  )
+  load_date = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+  bronze_cart_header.write.mode("append").parquet(
+      f"s3a://ecomerce-bronze123/bronze_cart_header/load_date={load_date}/"
+  )
+
+
+# Getting line items for carts
+def carts_line_items(carts):
+
+    # Flatten carts into individual products
+    cart_items = carts.select(
+        col("id").cast("int").alias("cart_id"),
+        col("userId").cast("int").alias("user_id"),
+        explode("products").alias("product")
+    )
+
+    # Select required fields from exploded structure
+    bronze_cart_items = cart_items.select(
+        col("cart_id").cast("int"),
+        col("user_id").cast("int"),
+        col("product.id").cast("int").alias("product_id"),
+        col("product.title").cast("string").alias("title"),
+        col("product.price").cast("double").alias("price"),
+        col("product.quantity").cast("int").alias("quantity"),
+        col("product.total").cast("double").alias("line_total"),
+        col("product.discountedTotal").cast("double").alias("discounted_total"),
+        col("product.discountPercentage").cast("double").alias("discount_pct")
+    )
+
+    # Load timestamp partition
+    load_date = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # Write to S3 (Bronze layer)
+    bronze_cart_items.write \
+        .mode("append") \
+        .parquet(
+            f"s3a://ecomerce-bronze123/bronze_cart_items/load_date={load_date}/"
+        )
+
+
+
+
+cart_header_create(carts_df)
+carts_line_items(carts_df)
+
+
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -1,27 +1,58 @@
 import boto3
 from dotenv import load_dotenv
+import os
+from pyspark.sql import SparkSession
 
-#create an s3 client
+load_dotenv()
+
+# Get credentials safely using getenv
+aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+
+# -------------------
+# S3 CLIENT
+# -------------------
 s3_client = boto3.client(
-        's3',
-        region_name='us-east-1',
-        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
-    )
+    "s3",
+    region_name="us-east-1",
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key
+)
 
-#create an glue client
+# -------------------
+# GLUE CLIENT
+# -------------------
 glue_client = boto3.client(
-    'glue',
-    region_name='us-east-1',
-    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+    "glue",
+    region_name="us-east-1",
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key
 )
 
-
-#create an athena client
+# -------------------
+# ATHENA CLIENT
+# -------------------
 athena_client = boto3.client(
-    'athena',
-    region_name='us-east-1',
-    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+    "athena",
+    region_name="us-east-1",
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key
 )
+
+
+# -------------------
+# SPARK SESSION
+# -------------------
+def get_spark():
+    return SparkSession.builder \
+        .appName("ecommerce_pipeline") \
+        .master("local[2]") \
+        .config("spark.driver.host", "127.0.0.1") \
+        .config("spark.driver.bindAddress", "127.0.0.1") \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.hadoop.fs.s3a.aws.credentials.provider",
+                "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+        .config("spark.jars.packages",
+                "org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262") \
+        .getOrCreate()
